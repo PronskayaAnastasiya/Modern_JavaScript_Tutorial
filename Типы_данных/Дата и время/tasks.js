7100be682822287777c39f47a65e3ee2ce28993e
PronskayaAnastasiya <100492707+PronskayaAnastasiya@users.noreply.github.com>
2022-07-20 22:12:27 +0300

    Update tasks.js

--- Типы_данных/Дата и время/tasks.js
+++ Типы_данных/Дата и время/tasks.js
@@ -131,3 +131,74 @@ getSecondsToday() == 36000 // (3600 * 10)
 Функция должна работать в любой день, т.е. в ней не должно быть конкретного 
 значения сегодняшней даты.
 */
+
+function getSecondsToday() {
+    let now = new Date();
+    let today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    let diff = now - today;
+    return Math.round(diff / 1000);
+}
+console.log(getSecondsToday());
+
+function getSecondsToTomorrow() {
+    let now = new Date();
+    let tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
+    let diff = tomorrow - now;
+    return Math.round(diff / 1000);
+}
+console.log(getSecondsToTomorrow());
+
+/* 
+!Форматирование относительной даты
+
+Напишите функцию formatDate(date), форматирующую date по следующему принципу:
+
+Если спустя date прошло менее 1 секунды, вывести "прямо сейчас".
+В противном случае, если с date прошло меньше 1 минуты, вывести "n сек. назад".
+В противном случае, если меньше часа, вывести "m мин. назад".
+В противном случае, полная дата в формате "DD.MM.YY HH:mm". А именно: "день.месяц.год часы:минуты", всё в виде двух цифр, т.е. 31.12.16 10:00.
+Например:
+
+alert( formatDate(new Date(new Date - 1)) ); // "прямо сейчас"
+
+alert( formatDate(new Date(new Date - 30 * 1000)) ); // "30 сек. назад"
+
+alert( formatDate(new Date(new Date - 5 * 60 * 1000)) ); // "5 мин. назад"
+
+вчерашняя дата вроде 31.12.2016, 20:00
+alert( formatDate(new Date(new Date - 86400 * 1000)) );
+*/
+
+function formatDate(date) {
+    let diff = new Date() - date; // разница в миллисекундах
+    if (diff < 1000) {
+        return "прямо сейчас";
+    }
+    let sec = Math.floor(diff / 1000);
+    if (sec < 60) {
+        return `${sec} сек. назад`
+    }
+    let min = Math.floor(diff / 60000);
+    if (min < 60) {
+        return `${min} мин. назад`
+    }
+    let d = date;
+    d = [
+        '0' + d.getDate(),
+        '0' + (d.getMonth() + 1),
+        '' + d.getFullYear(),
+        '0' + d.getHours(),
+        '0' + d.getMinutes()
+    ].map(component => component.slice(-2)); // взять последние 2 цифры из каждой компоненты
+
+    // соединить компоненты в дату
+    return d.slice(0, 3).join('.') + ' ' + d.slice(3).join(':');
+}
+
+console.log(formatDate(new Date(new Date - 1)));
+console.log(formatDate(new Date(new Date - 30 * 1000))); // "30 сек. назад"
+
+console.log(formatDate(new Date(new Date - 5 * 60 * 1000))); // "5 мин. назад"
+
+// вчерашняя дата вроде 31.12.2016, 20:00
+console.log(formatDate(new Date(new Date - 86400 * 1000)));
