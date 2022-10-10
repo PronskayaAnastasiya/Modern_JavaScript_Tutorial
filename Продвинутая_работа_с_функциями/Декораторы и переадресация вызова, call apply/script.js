a0710b5b71b05da23b459b886608d3500b6821b3
PronskayaAnastasiya <100492707+PronskayaAnastasiya@users.noreply.github.com>
2022-10-11 02:26:39 +0300

    Update script.js

--- Продвинутая_работа_с_функциями/Декораторы и переадресация вызова, call apply/script.js
+++ Продвинутая_работа_с_функциями/Декораторы и переадресация вызова, call apply/script.js
@@ -1,9 +1,10 @@
 //!Декораторы и переадресация вызова, call/apply
 
+
+
 //!Прозрачное кеширование
 
 /*
-
 function slow(x) {
     console.log(`Called with ${x}`);
     return x;
@@ -11,54 +12,91 @@ function slow(x) {
 
 function cachingDecorator(func) {
     let cache = new Map();
+
     return function (x) {
         if (cache.has(x)) {
             return cache.get(x);
         }
+
         let result = func(x);
         cache.set(x, result);
         return result;
-    };
+    }
 }
 
 slow = cachingDecorator(slow);
-
-console.log(slow(1)); // slow(1) кешируем
-console.log("Again: " + slow(1)); // возвращаем из кеша
-
-console.log(slow(2)); // slow(2) кешируем
-console.log("Again: " + slow(2)); // возвращаем из кеша
 */
 
 //!Применение «func.call» для передачи контекста.
 
-let worker = {
-
+let worker2 = {
     someMethod() {
-        return 1;
+        return 2;
     },
-
     slow(x) {
-        console.log(`Called with ${x}`);
+        console.log("Called with " + x);
         return x * this.someMethod();
     }
-}
+};
 
-function cachingDecorator(func) {
+function cachingDecorator2(func) {
     let cache = new Map();
     return function (x) {
         if (cache.has(x)) {
             return cache.get(x);
         }
-
-
         let result = func.call(this, x);
-
         cache.set(x, result);
         return result;
     };
 }
 
-console.log(worker.slow(1));
-worker.slow = cachingDecorator(worker.slow);
-console.log(worker.slow(2));
+worker2.slow = cachingDecorator2(worker2.slow);
+
+function sayHi() {
+    console.log(this.name);
+}
+let user = { name: "Tom" };
+let admin = { name: "John" };
+
+
+
+
+//!Переходим к нескольким аргументам с «func.apply»
+
+let worker3 = {
+    slow(min, max) {
+        console.log(`Called with ${min},${max}`);
+        return min + max;
+    }
+};
+
+function cachingDecorator3(func, hash) {
+    let cache = new Map();
+    return function () {
+        let key = hash(arguments);
+        if (cache.has(key)) {
+            return cache.get(key);
+        }
+        let result = func.call(this, ...arguments);
+        cache.set(key, result);
+        return result;
+    }
+}
+
+function hash(args) {
+    return args[0] + ',' + args[1];
+}
+
+worker3.slow = cachingDecorator3(worker3.slow, hash);
+console.log(worker3.slow(3, 5)); // работает
+console.log("Again " + worker3.slow(3, 5)); // аналогично (из кеша)
+
+
+//!Заимствование метода
+
+function hash4(args) {
+    return [].join.call(arguments);
+}
+
+
