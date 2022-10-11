ba2e4b5117b569b84c0f15a1d298786d68050240
PronskayaAnastasiya <100492707+PronskayaAnastasiya@users.noreply.github.com>
2022-10-11 21:10:44 +0300

    Update tasks.js

--- Продвинутая_работа_с_функциями/Декораторы и переадресация вызова, call apply/tasks.js
+++ Продвинутая_работа_с_функциями/Декораторы и переадресация вызова, call apply/tasks.js
@@ -65,9 +65,7 @@ f1500("test"); // показывает "test" после 1500 мс
 В приведённом выше коде f – функция с одним аргументом, но ваше решение 
 должно передавать все аргументы и контекст this.
 */
-function f(x) {
-  console.log(x);
-}
+
 
 function delay(func, ms) {
   return function (...args) {
@@ -78,6 +76,91 @@ function delay(func, ms) {
   };
 }
 
-let f1000 = delay(f, 1000);
-f1000("test"); // показывает "test" после 1000 мс
-f1000("gkjdfj", "kjlkg");
+
+/*
+!Декоратор debounce
+
+Результатом декоратора debounce(f, ms) должна быть обёртка, которая передаёт 
+вызов f не более одного раза в ms миллисекунд. Другими словами, когда мы 
+вызываем debounce, это гарантирует, что все остальные вызовы будут игнорироваться 
+в течение ms.
+
+Например:
+
+let f = debounce(alert, 1000);
+
+f(1); // выполняется немедленно
+f(2); // проигнорирован
+
+setTimeout( () => f(3), 100); // проигнорирован (прошло только 100 мс)
+setTimeout( () => f(4), 1100); // выполняется
+setTimeout( () => f(5), 1500); // проигнорирован (прошло только 400 мс от последнего вызова)
+На практике debounce полезен для функций, которые получают/обновляют данные, и мы знаем, что повторный вызов в течение короткого промежутка времени не даст ничего нового. Так что лучше не тратить на него ресурсы.
+*/
+function f(x) {
+  console.log(x);
+}
+
+function debounce(f, ms) {
+
+  let isCooldown = false;
+
+  return function () {
+    if (isCooldown) return;
+
+    f.apply(this, arguments);
+
+    isCooldown = true;
+
+    setTimeout(() => isCooldown = false, ms);
+  };
+
+}
+
+
+/*
+!Тормозящий (throttling) декоратор
+
+Создайте «тормозящий» декоратор throttle(f, ms), который возвращает обёртку,
+передавая вызов в f не более одного раза в ms миллисекунд. Те вызовы, 
+которые попадают в период «торможения», игнорируются.
+
+Отличие от debounce – если проигнорированный вызов является последним во 
+
+время «задержки», то он выполняется в конце.
+function f(a) {
+  console.log(a)
+}
+
+ f1000 передаёт вызовы f максимум раз в 1000 мс
+let f1000 = throttle(f, 1000);
+
+f1000(1); // показывает 1
+f1000(2); // (ограничение, 1000 мс ещё нет)
+f1000(3); // (ограничение, 1000 мс ещё нет)
+
+ когда 1000 мс истекли ...
+ ...выводим 3, промежуточное значение 2 было проигнорировано
+*/
+
+function throttle(f, ms) {
+  let isThrottled = false,
+    savedArgs,
+    savedThis;
+  function wrapper() {
+    if (isThrottled) {
+      savedArgs = arguments;
+      savedThis = this;
+      return;
+    }
+    f.apply(this, arguments);
+    isThrottled = true;
+    setTimeout(function () {
+      isThrottled = false;
+      if (savedArgs) {
+        wrapper.apply(savedThis, savedArgs);
+        savedArgs = savedThis = null;
+      }
+    }, ms);
+  }
+}
