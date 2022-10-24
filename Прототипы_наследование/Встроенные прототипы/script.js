780c2f72a19675330dd05f235cd5988def224f24
PronskayaAnastasiya <100492707+PronskayaAnastasiya@users.noreply.github.com>
2022-10-24 22:44:06 +0300

    Update script.js

--- Прототипы_наследование/Встроенные прототипы/script.js
+++ Прототипы_наследование/Встроенные прототипы/script.js
@@ -2,34 +2,22 @@ let obj = {};
 console.log(obj);
 
 console.log(obj.__proto__ === Object.prototype);
-
 console.log(Object.prototype.__proto__);
 
 let arr = [1, 2, 3];
 console.log(arr.__proto__ === Array.prototype);
 console.log(arr.__proto__.__proto__ === Object.prototype);
 console.log(arr.__proto__.__proto__.__proto__);
-console.dir(arr);
 
 String.prototype.show = function () {
     console.log(this);
 };
-
-"BOOM!".show(); // BOOM!
-
-if (!String.prototype.repeat) {
-    String.prototype.repeat = function (n) {
-        return new Array(n + 1).join(this);
-    }
-}
-console.log("LA".repeat(3));
+"BOOM".show();
 
 let obj2 = {
     0: "Hello",
     1: "world!",
-    length: 2,
+    length: 2
 };
-
 obj2.join = Array.prototype.join;
-
-console.log(obj2.join(',')); // Hello,world!
+console.log(obj2.join(','));
