e8f2a48c504db358502ea1b3f4afe3a6fe46a0bd
PronskayaAnastasiya <100492707+PronskayaAnastasiya@users.noreply.github.com>
2022-11-09 19:13:57 +0300

    проверка класса instanceof

--- Классы/Проверка класса instanceof/script.js
+++ Классы/Проверка класса instanceof/script.js
@@ -1,13 +1,18 @@
-class Rabbit { };
+class Rabbit { }
 let rabbit = new Rabbit();
-console.log(rabbit instanceof Rabbit);
 
-function Cat() { }
-console.log(new Cat() instanceof Cat);
+console.log(rabbit instanceof Rabbit);
+console.log(rabbit instanceof Object);
+
+
+function Dog() { };
+console.log(new Dog() instanceof Dog);
 
 let arr = [1, 2, 3];
 console.log(arr instanceof Array);
 console.log(arr instanceof Object);
+
+
 
 class Animal {
     static [Symbol.hasInstance](obj) {
@@ -17,6 +22,40 @@ class Animal {
 
 let obj = { canEat: true };
 console.log(obj instanceof Animal);
+
+
+function Cat() { };
+let cat = new Cat();
+Cat.prototype = {};
+console.log(cat instanceof Cat);
+
+
+
+
+let obj2 = {};
+console.log(obj2);
+console.log(obj.toString());
+
+
+let objectToString = Object.prototype.toString;
+let arr2 = [];
+console.log(objectToString.call(arr));
+console.log(objectToString.call(123));
+console.log(objectToString.call(alert));
+console.log(objectToString.call(null));
+
+let user = {
+    [Symbol.toStringTag]: "User"
+};
+console.log(objectToString.call(user));
+
+
+console.log(window[Symbol.toStringTag]);
+console.log(XMLHttpRequest.prototype[Symbol.toStringTag]);
+console.log(objectToString.call(window));
+console.log(objectToString.call(new XMLHttpRequest()));
+
+
 
 
 
